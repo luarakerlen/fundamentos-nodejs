a5778171adade8cce69c4033862d926f8154b02f
Luara Kerlen dos Santos Rangel <luarakerlen@hotmail.com>
2024-07-20 15:18:22 -0300

    initial commit

---
+++ src/server.js
@@ -0,0 +1,11 @@
+import http from 'node:http';
+
+const server = http.createServer((req, res) => {
+	const { method, url } = req;
+
+	console.log(`Method: ${method} | URL: ${url}`);
+
+	return res.end('Hello World');
+});
+
+server.listen(3333);
