20a40ff513c7f9d743e8b42504bcf9e1c5bf8e1a
Luara Kerlen dos Santos Rangel <luarakerlen@hotmail.com>
2024-07-24 19:50:52 -0300

    feat: Persist database changes to disk

--- src/database.js
+++ src/database.js
@@ -1,5 +1,23 @@
+import fs from 'node:fs/promises';
+
+const databasePath = new URL('db.json', import.meta.url);
+
 export class Database {
 	#database = {};
+
+	constructor() {
+		fs.readFile(databasePath.pathname, 'utf-8')
+			.then((data) => {
+				this.#database = JSON.parse(data);
+			})
+			.catch(() => {
+				this.#persist();
+			});
+	}
+
+	#persist() {
+		fs.writeFile(databasePath.pathname, JSON.stringify(this.#database));
+	}
 
 	select(table) {
 		const data = this.#database[table] ?? [];
@@ -8,12 +26,13 @@ export class Database {
 	}
 
 	insert(table, data) {
-		if (Array.isArray(this.database[table])) {
+		if (Array.isArray(this.#database[table])) {
 			this.#database[table].push(data);
 		} else {
 			this.#database[table] = [data];
 		}
 
+		this.#persist();
 		return data;
 	}
 }
