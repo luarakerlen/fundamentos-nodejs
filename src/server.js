f3d6dd848a4336a2187a38a07ab1978ae0b373cc
Luara Kerlen dos Santos Rangel <luarakerlen@hotmail.com>
2024-07-31 21:41:45 -0300

    feat: Add delete functionality for users

--- src/server.js
+++ src/server.js
@@ -7,11 +7,14 @@ const server = http.createServer(async (req, res) => {
 
 	await json(req, res);
 
-	const route = routes.find(route => {
-		return route.method === method && route.path === url
-	})
+	const route = routes.find((route) => {
+		return route.method === method && route.path.test(url);
+	});
 
-	if(route) {
+	if (route) {
+		const routeParams = req.url.match(route.path);
+		req.params = { ...routeParams.groups };
+
 		return route.handler(req, res);
 	}
 
